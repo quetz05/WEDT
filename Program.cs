91e9468ba102d9345c9e0f68dbd30a1aa6cff9dd
Quetz <quetz05@gmail.com>
2015-01-10 12:51:23 +0100

    Dodanie klasy parsera, ktora po wybraniu xpataha i dokumentu zwraca tablice Stringu zawartosci

--- Program.cs
+++ Program.cs
@@ -10,6 +10,17 @@ namespace WEDT
     {
         static void Main(string[] args)
         {
+            String docPath = "../../simple.xml";
+            String xpath = "//breakfast_menu/food/name";
+
+
+            String [] data = XMLParser.ParseDocument(docPath, xpath);
+
+            foreach (String line in data)
+                Console.WriteLine(line);
+
+            Console.ReadLine();
+
         }
     }
 }
---
+++ XMLParser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.XPath;
+
+namespace WEDT
+{
+    static class XMLParser
+    {
+        static public String[] ParseDocument(String docPath, String xpath)
+        {
+            List<String> data = new List<String>();
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(docPath);
+            XmlNodeList nodes = xmlDoc.SelectNodes(xpath);
+
+            foreach (XmlNode node in nodes)
+                data.Add(node.InnerText);
+
+            return data.ToArray();
+        }
+        
+
+    }
+}
