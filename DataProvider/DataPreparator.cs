1c524a2edaa3f3c0a8f11ad8b1a7b927cf0a38e8
WezSieTato <le0n.uo@gmail.com>
2015-01-13 01:37:05 +0100

    #6: DataProvider.WikiPagelinksProvider

---
+++ DataProvider/DataPreparator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WEDT.DataProvider
+{
+    class DataPreparator
+    {
+        static public String utfConversion(String str)
+        {
+            Regex rx = new Regex(@"\\[uU]([0-9A-F]{4})");
+            String matchString = rx.Replace(str, match => ((char)Int32.Parse(match.Value.Substring(2), NumberStyles.HexNumber)).ToString());
+            return matchString;
+        }
+
+        public static String FirstCharToUpper(String input)
+        {
+            if (String.IsNullOrEmpty(input))
+                throw new ArgumentException("ARGH!");
+            return input.First().ToString().ToUpper() + input.Substring(1);
+        }
+    }
+}
---
+++ DataProvider/WikiPagelinksProvider.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VDS.RDF;
+using VDS.RDF.Query;
+using VDS.RDF.Storage;
+
+namespace WEDT.DataProvider
+{
+    public class WikiPagelinksProvider
+    {
+        SparqlConnector store;
+
+        public WikiPagelinksProvider()
+        {
+            store = new SparqlConnector(new Uri("http://pl.dbpedia.org/sparql"));
+        }
+
+        public List<String> pagelinks(String from)
+        {
+           
+            List<String> list = new List<String>();
+
+            String strQuery;// = String.Format(strFormat, from);
+            strQuery = "SELECT * WHERE { <http://pl.dbpedia.org/resource/"
+                + DataPreparator.FirstCharToUpper(from) 
+                + "> <http://dbpedia.org/ontology/wikiPageWikiLink> ?a . }";
+            Object results = store.Query(strQuery);
+            if (results is SparqlResultSet)
+            {
+                SparqlResultSet rset = (SparqlResultSet)results;
+                foreach (SparqlResult r in rset)
+                {
+                    String str = (r["a"].ToString());
+                    str = str.Remove(0, 31);
+                    str = Uri.UnescapeDataString(str);
+                    list.Add(str);
+                }
+            }
+            else
+            {
+                throw new Exception("Did not get a SPARQL Result Set as expected");
+            }
+
+            return list;
+        }
+    }
+}
