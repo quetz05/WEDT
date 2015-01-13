90c68b10fc6cd8e1f237c5472e8b78841ed6bbff
Quetz <quetz05@gmail.com>
2015-01-13 21:42:17 +0100

    Gotowa klasa drzewa

--- Tree.cs
+++ Tree.cs
@@ -6,36 +6,70 @@ using System.Threading.Tasks;
 
 namespace WEDT
 {
-    delegate void TreeVisitor<T>(T nodeData);
-
-    class Tree<T>
-    {
-        T data;
-        LinkedList<Tree<T>> children;
-
-        public Tree(T data)
+        class Tree
         {
-            this.data = data;
-            children = new LinkedList<Tree<T>>();
-        }
 
-        public void addChild(T data)
-        {
-            children.AddFirst(new Tree<T>(data));
-        }
+            public String data;
+            LinkedList<Tree> children;
 
-        public Tree<T> getChild(int i)
-        {
-            foreach (Tree<T> n in children)
-                if (--i == 0) return n;
-            return null;
-        }
+            public Tree(String data)
+            {
+                this.data = data;
+                children = new LinkedList<Tree>();
+            }
 
-        public void traverse(Tree<T> node, TreeVisitor<T> visitor)
-        {
-            visitor(node.data);
-            foreach (Tree<T> kid in node.children)
-                traverse(kid, visitor);
+            public void addChild(String data)
+            {
+                children.AddFirst(new Tree(data));
+            }
+
+            public void addChilds(String[] data)
+            {
+                foreach (String d in data)
+                    children.AddFirst(new Tree(d));
+            }
+
+
+            public Tree getChild(int i)
+            {
+                foreach (Tree n in children)
+                    if (--i == 0) return n;
+                return null;
+            }
+
+
+
+            public Tree getChild(String s)
+            {
+                foreach (Tree n in children)
+                    if (n.data == s) return n;
+                return null;
+            }
+
+            //public void traverse(Tree node, TreeVisitor visitor)
+            //{
+            //    visitor(node.data);
+            //    foreach (Tree kid in node.children)
+            //        traverse(kid, visitor);
+            //}
+
+
+            public static void print(Tree node, String tab = "")
+            {
+                Console.WriteLine(tab + node.data);
+                tab += "-";
+                foreach (Tree kid in node.children)
+                    print(kid, tab);
+            }
+
+            public static bool FindCommon(Tree tree1, Tree tree2)
+            {
+
+
+
+                return true;
+
+            }
+
         }
-    }
 }
