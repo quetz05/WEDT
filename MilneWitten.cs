854d0989701b63f529bb09165f75ae6980fa1cb8
Quetz <quetz05@gmail.com>
2015-01-14 20:01:40 +0100

    Rozwiniecie algorytmu MilneWitten

--- MilneWitten.cs
+++ MilneWitten.cs
@@ -5,8 +5,23 @@ using System.Text;
 using System.Threading.Tasks;
 using WEDT.DataProvider;
 
+
+
 namespace WEDT
 {
+
+    class ArticlePair
+    {
+        public ArticlePair(int f, int s)
+        {
+            first = f;
+            second = s;
+        }
+        public int first;
+        public int second;
+
+    }
+
     class MilneWitten
     {
         String word1;
@@ -19,7 +34,7 @@ namespace WEDT
 
         int word1Path;
         int word2Path;
-
+        int amountLinks;
 
         private String word1Meaning;
         private String word2Meaning;
@@ -34,6 +49,9 @@ namespace WEDT
 
             word1Path = -1;
             word2Path = -1;
+
+            // TODO Zmienić na prawdziwą wartość
+            amountLinks = 10000;
         }
 
         private List<String> GetMeanings(String wordRedirect)
@@ -47,6 +65,31 @@ namespace WEDT
             return lexicalAssociationList;
         }
 
+
+        private List<double> GetVector(List<String> ownLinks, List<String> allLinks)
+        {
+            List<double> vec = new List<double>();
+            foreach(String link in allLinks)
+            {
+                double value = 0;
+                if(ownLinks.Contains(link))
+                    // TODO zmienić na poprawną funkcję
+                    value = Math.Log(amountLinks / wpp.pagelinks(link).Length);
+
+                vec.Add(value);
+            }
+
+            return vec;
+        }
+
+
+        // TODO NIE ROZUMIĘ JAK!!?
+        private double GetCosinus(double a, double b)
+        {
+            return 0.0;
+
+
+        }
 
         // Step 1
         private bool ChooseMeaning()
@@ -62,43 +105,32 @@ namespace WEDT
 
             List<String> lexicalAssociationList1 = GetMeanings(word1Redirects);
             List<String> lexicalAssociationList2 = GetMeanings(word2Redirects);
+            List<String> lexAssList = new List<String>();
+
+            foreach (String w in lexicalAssociationList1)
+                lexAssList.Add(w);
+
+            foreach (String w in lexicalAssociationList2)
+                lexAssList.Add(w);
+
 
            //Step 1.5
-           double maxAngle = 0;
-           int amountLinks = 10000;
+            List<double> vec1 = GetVector(lexicalAssociationList1, lexAssList);
+            List<double> vec2 = GetVector(lexicalAssociationList2, lexAssList);
 
-           //foreach (String m1 in lexicalAssociationList1)
-           //    foreach(String m2 in lexicalAssociationList2)
-           //    {
-           //        String [] links = wpp.pagelinks(m1);
-           //        double angle = Math.Log(amountLinks/);
+            Dictionary<double, ArticlePair> cosVec = new Dictionary<double, ArticlePair>();
 
 
-           //    }
+            // Step 1.75
+           for (int i = 0; i<vec1.Count; i++)
+               for (int j = 0; j<vec2.Count; j++)
+               {
+                   cosVec.Add(GetCosinus(vec1[i], vec2[j]), new ArticlePair(i, j));
+               }
 
-            //List<String> lexicalCroppedList1 = GetCroppedMeanings(lexicalAssociationList1.ToArray());
-            //List<String> lexicalCroppedList2 = GetCroppedMeanings(lexicalAssociationList2.ToArray());
+           var max = cosVec.Max();
 
-            //var intersect = lexicalCroppedList1.Intersect(lexicalCroppedList2, StringComparer.OrdinalIgnoreCase);
-            //String[] intersected = intersect.ToArray<String>();
-
-            //if (intersected.Length == 0)
-            //{
-            //    if (lexicalAssociationList1.Count > 0)
-            //        word1Meaning = lexicalAssociationList1[0];
-            //    else
-            //        word1Meaning = word1Redirects;
-
-            //    if (lexicalAssociationList2.Count > 0)
-            //        word2Meaning = lexicalAssociationList2[0];
-            //    else
-            //        word2Meaning = word2Redirects;
-            //}
-            //else
-            //{
-            //    word1Meaning = FindWord(intersected[0], lexicalAssociationList1);
-            //    word2Meaning = FindWord(intersected[0], lexicalAssociationList2);
-            //}
+          // ArticlePair articles = max.Value;
 
             return true;
         }
